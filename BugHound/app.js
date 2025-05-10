41fb3c97f1e188b85ba1d7c1a4f9549540b19573
Sivaraman-msc <sivaraman.bca.st@gmail.com>
2025-05-10 22:06:33 +0530

    CORS updated

--- BugHound/app.js
+++ BugHound/app.js
@@ -16,7 +16,7 @@ DbConnect()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 app.use(cookieParser()) 
-app.use(cors({origin : "http://localhost:5173",credentials:true}))
+app.use(cors({origin : "https://bug-hound-88g7.vercel.app",credentials:true}))
 app.use(morgan('dev')) 
 app.use(compression())
 /*const limit=rateLimit({
